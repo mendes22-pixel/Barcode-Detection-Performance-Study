3be5d7f7349dde179479ba869ded46a3a6d7acf2
Rui Mendes <ruipedro.mendes22@gmail.com>
2026-04-01 15:07:21 +0100

    Update YOLO_Training.py

--- Code/YOLO_Training.py
+++ Code/YOLO_Training.py
@@ -1,1 +1,13 @@
+from ultralytics import YOLO
+
+if __name__ == '__main__':
+    model = YOLO('yolov8n.pt')
+    model.train(
+        data='Dataset/YOLOv8/data.yaml',
+        epochs=20,
+        imgsz=640,
+        batch=4,
+        device='cuda'  
+    )
+
 
